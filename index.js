e5d65ed3fe1a78a775ed2f35749c1627ba740abd
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-09-29 21:01:55 +0600

    middleware done

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
-
 import express from 'express';
 import mongodbConnection from './db/index.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 const app=express();
-mongodbConnection();
-app.use(express.json());
+
+//middleware
+app.use(express.json({limit:"2MB"}));
+app.use(express.urlencoded({extended:true , limit:"2MB"}));
+app.use(express.static('public'));
+app.use(cors());
+app.use(cookieParser());
+
+mongodbConnection().then(()=>{
+  app.listen((process.env.PORT || 3000),()=>{
+    console.log(`http://localhost:${process.env.PORT || 3000}`);
+})
+}).catch((err)=>{
+throw new err;
+})
+
 app.get('/',(req,res)=>{
   res.send("Server is running");
-})
-app.listen((process.env.PORT || 3000),()=>{
-    console.log(`http://localhost:${process.env.PORT || 3000}`);
 })
